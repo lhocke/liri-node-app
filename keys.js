1b11eff526a536ed117566b12b054b82fc33eac5
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-10 14:29:27 -0700

    initial file commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'xk4IBus6TOpVIty23ZKjaPgV9',
+  consumer_secret: 'e47c6ycJ2VQgRf1yyG0ZmU1uZyl9jEfxKWeYi9O1h0scGpkmYC',
+  access_token_key: '18133525-XpzsN6ALYhPk8nt2Z2CqfpM08Rja9Ypv3tXTVWQxe',
+  access_token_secret: 'F5CFsi6RUQP9FgMVc87WhIiFJPJFvx4oTvAMPEHEm9mNZ'
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+// creating necessary requirements
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+var request = require('request');
+var fs = require('fs');
+
+// new variables
