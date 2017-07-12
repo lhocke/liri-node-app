9f474e0544f7a7b2b5540d3fbb48cb3ae2943868
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-07-12 13:37:05 -0700

    commiting to preserve

--- liri.js
+++ liri.js
@@ -11,13 +11,14 @@ var spotifyKeys = keys.spotifyKeys;
 var query = process.argv.slice(3);
 console.log(query);
 
-switch(functionUsed){
+switch(functionUsed) {
     case "my-tweets":
         console.log("switch")
         myTweets()
         break;
     case "spotify-this-song":
         console.log("second switch")
+        spotifyThisSong()
         break;
     case "movie-this":
         console.log("third switch")
@@ -28,13 +29,13 @@ switch(functionUsed){
 }
 // core functions
 function myTweets() {
-	console.log("running")
+    console.log("running")
 // import keys
     var client = new Twitter({
         consumer_key: twitterKeys.consumer_key,
         consumer_secret: twitterKeys.consumer_secret,
         access_token_key: twitterKeys.access_token_key,
-        access_token_secret: twitterKeys.access_token_secret,
+        access_token_secret: twitterKeys.access_token_secret
     })
 // set the user to search
     if (query = undefined) {
@@ -43,25 +44,36 @@ function myTweets() {
 // grab the last 20 tweets and display with timestamps
     var params = {q: query, count: "20"}
     client.get('statuses/user_timeline', params, function(error, tweets, response){
-    	for (var i = 0; i < tweets.length; i++){
+        for (var i = 0; i < tweets.length; i++){
             console.log(i + 1 + ": " + tweets[i].text);
         }
         // console.log(tweets[0].text);
     })
 };
 
-function spotify-this-song() {
+function spotifyThisSong() {
 // import keys
-    var spotify = ({
+    var spotify = new Spotify({
         id: spotifyKeys.client_id,
         secret: spotifyKeys.client_secret
     });
 
 // construct objects for songs returned
-    var Song = function(artist, name, preview, album) {
-        this
+    // var Song = function(artist, name, preview, album) {
+    //     this
+    // }
+    if (query = undefined){
+        query = "The Sign"
+        var type =
     }
-};
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(data.tracks.items[0].name);
+        });
+    }
 
 // function movie-this() {
 // connect to omdbapi and get json info
@@ -87,11 +99,11 @@ function spotify-this-song() {
 // };
 
 // function do-what-it-says() {
-// 	fs.readFile("random.txt", "utf8", function(error, data){
+// fs.readFile("random.txt", "utf8", function(error, data){
 //       if (error){
 //         return console.log(error)
 //       }
 //       console.log(data)
-// 	})
+// })
 
 // };
